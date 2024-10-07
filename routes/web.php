05ae9389a00a58ca8f0283b80036b4b750bf10d7
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-10-07 11:26:13 +0700

    edit profile

--- routes/web.php
+++ routes/web.php
@@ -42,13 +42,13 @@ Route::middleware(['auth'])->group(function () {
         Route::post("/checkout", [OrderController::class, "createOrderPost"]);
     });
 
+    Route::prefix('profile')->group(function () {
+        Route::get('/', [UserProfileController::class, 'index'])->name('profile.index');
+        Route::post('/', [UserProfileController::class, 'update']);
+    });
+
     Route::middleware('role:SUPER')->prefix('dashboard')->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-
-        Route::prefix('profile')->group(function () {
-            Route::get('/', [UserProfileController::class, 'index'])->name('profile.index');
-            Route::post('/', [UserProfileController::class, 'update']);
-        });
 
         Route::prefix("master")->group(function () {
             Route::prefix("orders")->group(function () {
